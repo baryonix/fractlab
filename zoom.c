07b9d7264d19eeb4edbaaeeb9741ee956e96ef9b
Jan Andres <jandres@gmx.net>
2006-12-21 23:43:31 +0100

    Make more settings configurable; change PNG output to use 8-bit colors.
    committer: Jan Andres <jandres@gmx.net>

--- zoom.c
+++ zoom.c
@@ -16,6 +16,9 @@
 #include "mandelbrot.h"
 
 
+#define DEFAULT_MAXITER 1000
+
+
 struct zoom_state {
 	mpfr_t x0, xn, d0, dn, a, ln_a, b, c;
 };
@@ -23,15 +26,25 @@ struct zoom_state {
 const int thread_count = 1;
 
 struct {
-	unsigned short r, g, b;
+	unsigned char r, g, b;
 } colors[COLORS];
 
 
 static gchar *start_coords = NULL, *target_coords = NULL;
+static gint maxiter = DEFAULT_MAXITER, frame_count = 0;
+static gdouble log_factor = 0.0;
+static gint img_width = 200, img_height = 200;
+
+static double aspect;
 
 static GOptionEntry option_entries [] = {
 	{"start-coords", 's', 0, G_OPTION_ARG_FILENAME, &start_coords, "Start coordinates", "FILE"},
 	{"target-coords", 't', 0, G_OPTION_ARG_FILENAME, &target_coords, "Target coordinates", "FILE"},
+	{"maxiter", 'i', 0, G_OPTION_ARG_INT, &maxiter, "Maximum # of iterations"},
+	{"frames", 'n', 0, G_OPTION_ARG_INT, &frame_count, "# of frames in animation"},
+	{"log-factor", 'l', 0, G_OPTION_ARG_DOUBLE, &log_factor, "Use logarithmic colors, color = LF * ln (iter)", "LF"},
+	{"width", 'W', 0, G_OPTION_ARG_INT, &img_width, "Image width", "PIXELS"},
+	{"height", 'H', 0, G_OPTION_ARG_INT, &img_height, "Image height", "PIXELS"},
 	{NULL}
 };
 
@@ -48,6 +61,10 @@ parse_command_line (int *argc, char ***argv)
 }
 
 
+/*
+ * Note that this function initializes the state for the calculation
+ * of frames 0 .. n, so we'll actually generate n + 1 frames.
+ */
 void
 init_zoom_state (struct zoom_state *state, mpf_t x0, mpf_t magf0, mpf_t xn, mpf_t magfn, unsigned long n)
 {
@@ -142,12 +159,12 @@ render_frame (struct zoom_state *xstate, struct zoom_state *ystate, unsigned lon
 	mpfr_add (tmp0, cfr, dfr, GMP_RNDN);
 	mpfr_get_f (md->ymax_f, tmp0, GMP_RNDN);
 
-	md->data = malloc (200 * 200 * sizeof (unsigned));
-	md->w = 200;
-	md->h = 200;
-	md->maxiter = 10000;
+	md->data = malloc (img_width * img_height * sizeof (unsigned));
+	md->w = img_width;
+	md->h = img_height;
+	md->maxiter = maxiter;
 	md->render_method = RM_MARIANI_SILVER;
-	md->log_factor = 0.0;
+	md->log_factor = log_factor;
 
 	/*char xminc[1024], xmaxc[1024], yminc[1024], ymaxc[1024];
 	coords_to_string (xmin, xmax, ymin, ymax, xminc, xmaxc, yminc, ymaxc, 1024);
@@ -183,16 +200,16 @@ write_png (const struct mandeldata *md, const char *filename)
 	if (setjmp (png_jmpbuf (png_ptr)))
 		fprintf (stderr, "PNG longjmp!\n");
 
-	png_set_swap (png_ptr); /* FIXME this should only be done on little-endian systems */
+	//png_set_swap (png_ptr); /* FIXME this should only be done on little-endian systems */
 	png_init_io (png_ptr, f);
 	png_set_filter (png_ptr, 0, PNG_FILTER_NONE);
 	png_set_compression_level (png_ptr, 0);
-	png_set_IHDR (png_ptr, info_ptr, md->w, md->h, 16, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+	png_set_IHDR (png_ptr, info_ptr, md->w, md->h, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
 		PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 	png_write_info (png_ptr, info_ptr);
 	//png_set_flush (png_ptr, 16);
 
-	unsigned short row[md->w * 3];
+	unsigned char row[md->w * 3];
 	png_bytep row_ptr[] = {(png_bytep) row};
 
 	unsigned int x, y;
@@ -231,6 +248,7 @@ main (int argc, char **argv)
 	mpfr_set_default_prec (1024); /* ! */
 	struct zoom_state xstate, ystate;
 	mpf_t cx0, cy0, magf0, cxn, cyn, magfn;
+	mpf_t mpaspect;
 	int i;
 	mpf_init (cx0);
 	mpf_init (cy0);
@@ -238,6 +256,7 @@ main (int argc, char **argv)
 	mpf_init (cxn);
 	mpf_init (cyn);
 	mpf_init (magfn);
+	mpf_init (mpaspect);
 	parse_command_line (&argc, &argv);
 	if (start_coords == NULL || !read_center_coords_from_file (start_coords, cx0, cy0, magf0)) {
 		fprintf (stderr, "* Error: No start coordinates specified.\n");
@@ -249,15 +268,26 @@ main (int argc, char **argv)
 	}
 
 	for (i = 0; i < COLORS; i++) {
-		colors[i].r = (unsigned short) (sin (2 * M_PI * i / COLORS) * 32767) + 32768;
-		colors[i].g = (unsigned short) (sin (4 * M_PI * i / COLORS) * 32767) + 32768;
-		colors[i].b = (unsigned short) (sin (6 * M_PI * i / COLORS) * 32767) + 32768;
+		colors[i].r = (unsigned short) (sin (2 * M_PI * i / COLORS) * 127) + 128;
+		colors[i].g = (unsigned short) (sin (4 * M_PI * i / COLORS) * 127) + 128;
+		colors[i].b = (unsigned short) (sin (6 * M_PI * i / COLORS) * 127) + 128;
 	}
 
-	init_zoom_state (&xstate, cx0, magf0, cxn, magfn, 100);
-	init_zoom_state (&ystate, cy0, magf0, cyn, magfn, 100);
+	aspect = (double) img_width / img_height;
+	mpf_set_d (mpaspect, aspect);
+	if (aspect > 1.0) {
+		init_zoom_state (&ystate, cy0, magf0, cyn, magfn, frame_count - 1);
+		mpf_div (magf0, magf0, mpaspect);
+		mpf_div (magfn, magfn, mpaspect);
+		init_zoom_state (&xstate, cx0, magf0, cxn, magfn, frame_count - 1);
+	} else {
+		init_zoom_state (&xstate, cx0, magf0, cxn, magfn, frame_count - 1);
+		mpf_mul (magf0, magf0, mpaspect);
+		mpf_mul (magfn, magfn, mpaspect);
+		init_zoom_state (&ystate, cy0, magf0, cyn, magfn, frame_count - 1);
+	}
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < frame_count; i++)
 		render_frame (&xstate, &ystate, i);
 
 	return 0;
