911ca1ff9c71f3395de556c633d4a2d05338aa85
Jan Andres <jandres@gmx.net>
2007-02-04 16:56:36 +0100

    Remove legacy file I/O code.
    committer: Jan Andres <jandres@gmx.net>

--- file.h
+++ file.h
@@ -6,7 +6,6 @@
 
 bool read_mandeldata (const char *filename, struct mandeldata *md, char *errbuf, size_t errbsize);
 bool fread_mandeldata (FILE *f, struct mandeldata *md, char *errbuf, size_t errbsize);
-bool fread_mandeldata_legacy (FILE *f, struct mandeldata *md);
 bool write_mandeldata (const char *filename, const struct mandeldata *md, char *errbuf, size_t errbsize);
 bool fwrite_mandeldata (FILE *f, const struct mandeldata *md, char *errbuf, size_t errbsize);
 
